ccc6134b6826a2a5d8c3cee67f2c45f886f46589
Cole Kashino <cjkashino@gmail.com>
2022-08-10 21:53:34 -0700

    mobile controls

--- Scripts/GlobalScript.gd
+++ Scripts/GlobalScript.gd
@@ -2,3 +2,5 @@ extends Node
 
 var score = 0
 var health = 3
+
+var touch_controls = false
--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -1,5 +1,11 @@
 extends Node2D
-
+onready var globals = get_node("/root/GlobalScript")
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://Scenes/MainGameNode.tscn")
+
+
+
+func _on_CheckBox_toggled(button_pressed):
+	globals.touch_controls = button_pressed
+	print(globals.touch_controls)
